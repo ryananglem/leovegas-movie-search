2016348303738724893b96221dacf5a9522aaeeb
Ryan Anglem <ryan.anglem@leovegas.com>
2021-04-01 08:21:41 +0200

    remove commented code

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,14 +6,3 @@ module.exports = {
       '@storybook/addon-docs'
     ]
   };
-
-  /*
-  import React from 'react'
-import { configure, addDecorator } from '@storybook/react';
-import BaseStyle from '../src/styles/baseStyle'
-
-addDecorator(s => <><BaseStyle />{s()}</>);
-
-configure(require.context('../src', true, /\.story\.tsx$/), module);
-
-*/
