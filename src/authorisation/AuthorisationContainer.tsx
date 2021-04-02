9df893a0b64507aa0a24b3ff1ec8e69a838c129a
Ryan Anglem <ryan.anglem@gmail.com>
2021-04-02 22:32:25 +0200

    remove extra import

--- src/authorisation/AuthorisationContainer.tsx
+++ src/authorisation/AuthorisationContainer.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect, useDispatch, useSelector } from 'react-redux'
 import qs from 'query-string'
-import { State } from '../store'
 import {
   authIdSelector,
   deniedAuthSelector,
