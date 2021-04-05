785f04072e8b125242d5c3bdfe52035d88f068ff
Ryan Anglem <ryan.anglem@gmail.com>
2021-04-05 14:01:48 +0200

    clean up e2e test

--- cypress/integration/detail.spec.js
+++ cypress/integration/detail.spec.js
@@ -8,19 +8,11 @@ describe('movie detail', () => {
   
     it('should open film detail page',  () => {
 
-        console.log('localstorage', window.localStorage.getItem('refreshToken'))
 
         fetch('https://api.themoviedb.org/3/authentication/guest_session/new?api_key=61d59ef5b346ef2091b6edfd3a3c8876').then(response => {
         
-            console.log('sessionResponse', response)
             response.json().then((session) => {
-
-          
-            console.log('session:', session)    
             window.localStorage.setItem('refreshToken', session.guest_session_id) 
-        
-            console.log('localstorage', window.localStorage.getItem('refreshToken'))
-        
             const filmId = 742
             cy.visit(`${config.website}/detail/${filmId}`)
 
